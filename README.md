b09134d1839b5ccea0b38046e65525906632153e
Maísa Amadeu <maisa.antonella.amadeu@gmail.com>
2023-11-20 13:58:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# desafio_tecnico_2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/desafio_tecnico_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.desafio_tecnico_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
