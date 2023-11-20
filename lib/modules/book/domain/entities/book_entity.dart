a8cddaa32ef577e92b1cd5cdc9c4b8be04bb3867
Maísa Amadeu <maisa.antonella.amadeu@gmail.com>
2023-11-20 14:05:09 -0300

    criação do book entity

---
+++ lib/modules/book/domain/entities/book_entity.dart
@@ -0,0 +1,13 @@
+class BookEntity {
+  final String title;
+  final String author;
+  final String coverUrl;
+  final String downloadUrl;
+
+  BookEntity({
+    required this.title,
+    required this.author,
+    required this.coverUrl,
+    required this.downloadUrl,
+  });
+}
