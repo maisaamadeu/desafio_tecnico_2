605ae4db580351efb5603b08a63f7c0209db5b48
Maísa Amadeu <maisa.antonella.amadeu@gmail.com>
2023-11-22 14:34:33 -0300

    setando a orientação do aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:desafio_tecnico_2/app_module.dart';
 import 'package:desafio_tecnico_2/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    ModularApp(
-      module: AppModule(),
-      child: const AppWidget(),
-    ),
-  );
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) {
+    runApp(
+      ModularApp(
+        module: AppModule(),
+        child: const AppWidget(),
+      ),
+    );
+  });
 }
